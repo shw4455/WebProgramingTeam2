ec0512d05e135f601acb2d9dacd56059736150ae
youngji617 <152296722+youngji617@users.noreply.github.com>
2024-06-14 23:41:18 +0900

    Update Select.js

--- src/component/youngji/component/Select.js
+++ src/component/youngji/component/Select.js
@@ -1,36 +1,117 @@
-import React,{ useState, useTransition } from "react";
-import View from "./View";
+import React, { useState } from "react";
 
-function Select(props){
-    
-    const [grain,setGrain] =useState('');
-    const [meat,setMeat] = useState('');
-    const [vege,setVege] = useState('');
-    const [fruit,setFruit] = useState('');
-    const [milk,setMilk] = useState('');
+function Select() {
+    const [answers, setAnswers] = useState({
+        grain: '',
+        meat: '',
+        vege: '',
+        fruit: '',
+        milk: ''
+    });
 
-    const handleChange = (event) =>{
-        setGrain(event.target.value);
-    }
-    const grainSubmit = (event) =>{
+    const [errors, setErrors] = useState({
+        grain: '',
+        meat: '',
+        vege: '',
+        fruit: '',
+        milk: ''
+    });
 
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        setAnswers(prevState => ({ ...prevState, [name]: value }));
+    };
+
+    const handleSubmit = (event, correctAnswer) => {
         event.preventDefault();
-        if(grain === "ㄱ"){
-            alert("정답");
+        const { name } = event.target.elements[0];
+
+        if (answers[name] === correctAnswer) {
+            setErrors(prevState => ({ ...prevState, [name]: '정답' }));
+        } else {
+            setErrors(prevState => ({ ...prevState, [name]: "틀렸습니다.다시 푸세요~" }));
         }
-        else{
-            alert("틀렸습니다.다시 푸세요~")
-        }
-    }
-    
-    return(
-        <form onSubmit={grainSubmit} style={{ textAlign: 'center' }}>
-            <label> 
-                <input type="text" value={grain} onChange={handleChange} style={{ width: '50px' }} />
-            </label>
-            <button type="submit">입력</button>
-            {/* {error && <p style={{ color: 'red' }}>{error}</p>} */}
-        </form>
+    };
+
+    return (
+        <>
+            <table style={{ margin: "auto", textAlign: "center", borderCollapse: "collapse" }}>
+                <thead>
+                    <tr style={{ border: "1px solid black" }}>
+                        <th style={{ border: "1px solid black", padding: "10px", backgroundColor: 'lightgreen' }}>식품군</th>
+                        <th style={{ border: "1px solid black", padding: "10px", backgroundColor: 'lightgreen' }}>보기 넣기</th>
+                        <th style={{ border: "1px solid black", padding: "10px", backgroundColor: 'lightgreen' }}>영양소</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr style={{ border: "1px solid black" }}>
+                        <td style={{ border: "1px solid black", padding: "10px" }}>1. 곡류</td>
+                        <td style={{ border: "1px solid black", padding: "10px" }}>
+                            <form onSubmit={(e) => handleSubmit(e, "ㄹ")} style={{ textAlign: 'center' }}>
+                                <label>
+                                    <input type="text" name="grain" value={answers.grain} onChange={handleChange} style={{ width: '50px' }} />
+                                </label>
+                                <button type="submit">입력</button>
+                                {errors.grain && <p style={{ color: 'red' }}>{errors.grain}</p>}
+                            </form>
+                        </td>
+                        <td style={{ border: "1px solid black", padding: "10px" }}>탄수화물</td>
+                    </tr>
+                    <tr style={{ border: "1px solid black" }}>
+                        <td style={{ border: "1px solid black", padding: "10px" }}>2. 고기, 생선류</td>
+                        <td style={{ border: "1px solid black", padding: "10px" }}>
+                            <form onSubmit={(e) => handleSubmit(e, "ㄷ")} style={{ textAlign: 'center' }}>
+                                <label>
+                                    <input type="text" name="meat" value={answers.meat} onChange={handleChange} style={{ width: '50px' }} />
+                                </label>
+                                <button type="submit">입력</button>
+                                {errors.meat && <p style={{ color: 'red' }}>{errors.meat}</p>}
+                            </form>
+                        </td>
+                        <td style={{ border: "1px solid black", padding: "10px" }}>단백질</td>
+                    </tr>
+                    <tr style={{ border: "1px solid black" }}>
+                        <td style={{ border: "1px solid black", padding: "10px" }}>3. 채소류</td>
+                        <td style={{ border: "1px solid black", padding: "10px" }}>
+                            <form onSubmit={(e) => handleSubmit(e, "ㅁ")} style={{ textAlign: 'center' }}>
+                                <label>
+                                    <input type="text" name="vege" value={answers.vege} onChange={handleChange} style={{ width: '50px' }} />
+                                </label>
+                                <button type="submit">입력</button>
+                                {errors.vege && <p style={{ color: 'red' }}>{errors.vege}</p>}
+                            </form>
+                        </td>
+                        <td style={{ border: "1px solid black", padding: "10px" }}>무기질</td>
+                    </tr>
+                    <tr style={{ border: "1px solid black" }}>
+                        <td style={{ border: "1px solid black", padding: "10px" }}>4. 과일류</td>
+                        <td style={{ border: "1px solid black", padding: "10px" }}>
+                            <form onSubmit={(e) => handleSubmit(e, "ㄱ")} style={{ textAlign: 'center' }}>
+                                <label>
+                                    <input type="text" name="fruit" value={answers.fruit} onChange={handleChange} style={{ width: '50px' }} />
+                                </label>
+                                <button type="submit">입력</button>
+                                {errors.fruit && <p style={{ color: 'red' }}>{errors.fruit}</p>}
+                            </form>
+                        </td>
+                        <td style={{ border: "1px solid black", padding: "10px" }}>비타민</td>
+                    </tr>
+                    <tr style={{ border: "1px solid black" }}>
+                        <td style={{ border: "1px solid black", padding: "10px" }}>5. 유제품류</td>
+                        <td style={{ border: "1px solid black", padding: "10px" }}>
+                            <form onSubmit={(e) => handleSubmit(e, "ㄴ")} style={{ textAlign: 'center' }}>
+                                <label>
+                                    <input type="text" name="milk" value={answers.milk} onChange={handleChange} style={{ width: '50px' }} />
+                                </label>
+                                <button type="submit">입력</button>
+                                {errors.milk && <p style={{ color: 'red' }}>{errors.milk}</p>}
+                            </form>
+                        </td>
+                        <td style={{ border: "1px solid black", padding: "10px" }}>칼슘</td>
+                    </tr>
+                </tbody>
+            </table>
+        </>
     );
 }
 
