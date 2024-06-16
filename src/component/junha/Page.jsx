23e2d2b23d0cf2025a1884fd74e0cc471e6c373a
서휘원 <shw445567@naver.com>
2024-06-16 22:04:45 +0900

    Junha Page, HanjaTable 추가

--- src/component/junha/Page.jsx
+++ src/component/junha/Page.jsx
@@ -4,10 +4,15 @@ import Question1 from "./components/Question1";
 import Question2 from "./components/Question2";
 import Question3 from "./components/Question3";
 import Question4 from "./components/Question4";
+import HanjaTable from "./components/HanjaTable.js";
 
 function Page(props) {
   return (
     <>
+      <HanjaTable />
+
+      <h1>예시: 二十三 + 四十五 = ?</h1>
+      <h1> 23 + 45 = 68</h1>
       <Question1></Question1>
       <Question2></Question2>
       <Question3></Question3>
